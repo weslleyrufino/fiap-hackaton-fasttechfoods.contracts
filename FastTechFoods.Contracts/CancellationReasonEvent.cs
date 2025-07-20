4dcafd1231bde8b87d7331cea52168c14de6ce60
Weslley Lima <weslley.lima@centauro.com.br>
2025-07-20 21:04:46 +0100

    Adicionado CancellationReasonEvent

---
+++ FastTechFoods.Contracts/CancellationReasonEvent.cs
@@ -0,0 +1,9 @@
+﻿using FastTechFoods.Contracts.Enum;
+
+namespace FastTechFoods.Contracts;
+public record CancellationReasonEvent
+(
+    Guid Id,
+    EnumStatus Status,
+    string CancellationReason
+    );
