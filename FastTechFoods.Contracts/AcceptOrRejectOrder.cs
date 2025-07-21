c09d3e3dbfc52e2382d666812aaec03878783520
Weslley Lima <weslley.lima@centauro.com.br>
2025-07-21 21:42:29 +0100

    AcceptOrRejectOrder object + v4

---
+++ FastTechFoods.Contracts/AcceptOrRejectOrder.cs
@@ -0,0 +1,9 @@
+﻿using FastTechFoods.Contracts.Enum;
+
+namespace FastTechFoods.Contracts;
+public record AcceptOrRejectOrder
+(
+    Guid Id,
+    EnumStatus Status,
+    string CancellationReason
+);
